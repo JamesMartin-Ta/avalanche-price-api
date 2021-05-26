b27024624cad67036e6056a340f5ad013711362e
James T <james-martin.ta@outlook.fr>
2021-05-26 21:19:22 +0200

    test

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const express = require('express');
 var cors = require('cors');
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'origin': '*',
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
+}));
 
 var TokenService = require('./services/TokenService');
-//allow OPTIONS on all resources
-app.options('*', cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.put('/avalanche/price/usdt', cors(), (req, res) => {
   try {
