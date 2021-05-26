910baf68c93d53e2fab4d56c1b3f41e5427f5f6c
James T <james-martin.ta@outlook.fr>
2021-05-26 22:00:08 +0200

    update cors

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ var cors = require('cors');
 const app = express();
 app.use(express.json());
 
-app.options('/*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.sendStatus(200);
-});
-
 var TokenService = require('./services/TokenService');
 
 app.put('/avalanche/price/usdt', cors(), (req, res) => {
