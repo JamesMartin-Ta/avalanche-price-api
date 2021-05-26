7b0f63d3174ae071434283af64a01f9c1175168f
James T <james-martin.ta@outlook.fr>
2021-05-26 21:27:00 +0200

    test

--- index.js
+++ index.js
@@ -2,18 +2,14 @@ const express = require('express');
 var cors = require('cors');
 const app = express();
 app.use(express.json());
-app.use((req, res, next) => {
-  //doesn't send response just adjusts it
-  res.header('Access-Control-Allow-Origin', '*'); //* to give access to any origin
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization' //to give access to all the headers provided
-  );
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); //to give access to all the methods provided
-    return res.status(200).json({});
-  }
-  next(); //so that other routes can take over
+
+app.options('*', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.send('ok');
+});
+
+app.use((req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
 });
 
 var TokenService = require('./services/TokenService');
