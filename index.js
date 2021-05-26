096cb402da608bd7ee3f7ab9ad2b34d0c2e4ce54
James T <james-martin.ta@outlook.fr>
2021-05-26 21:16:13 +0200

    test

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var TokenService = require('./services/TokenService');
 //allow OPTIONS on all resources
 app.options('*', cors());
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
