ca84ccc1c9ab941f8d9f5e6d9b65bb3b1f06d1ac
James T <james-martin.ta@outlook.fr>
2021-05-26 21:25:32 +0200

    test

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const express = require('express');
 var cors = require('cors');
 const app = express();
 app.use(express.json());
-app.use(cors({
-  'allowedHeaders': ['sessionId', 'Content-Type'],
-  'exposedHeaders': ['sessionId'],
-  'origin': '*',
-  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  'preflightContinue': false
-}));
+app.use((req, res, next) => {
+  //doesn't send response just adjusts it
+  res.header('Access-Control-Allow-Origin', '*'); //* to give access to any origin
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization' //to give access to all the headers provided
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); //to give access to all the methods provided
+    return res.status(200).json({});
+  }
+  next(); //so that other routes can take over
+});
 
 var TokenService = require('./services/TokenService');
-
 
 app.put('/avalanche/price/usdt', cors(), (req, res) => {
   try {
