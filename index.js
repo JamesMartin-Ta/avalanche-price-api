f2b580aad8dbb0e399399b14539b31a67ed4a43c
James T <james-martin.ta@outlook.fr>
2021-05-26 20:30:54 +0200

    cors

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 var cors = require('cors');
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: true, credentials: true }));
-app.options('*', cors());
+app.use(cors());
 
 var TokenService = require('./services/TokenService');
 
