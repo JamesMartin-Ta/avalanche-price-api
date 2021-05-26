183d38650e8c63b9a46804dbe93724d3c51953ca
James T <james-martin.ta@outlook.fr>
2021-05-26 22:08:26 +0200

    cors

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ var cors = require('cors');
 const app = express();
 app.use(express.json());
 
+app.all('/*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.sendStatus(200);
+});
+
 var TokenService = require('./services/TokenService');
 
 app.put('/avalanche/price/usdt', cors(), (req, res) => {
@@ -12,11 +19,11 @@ app.put('/avalanche/price/usdt', cors(), (req, res) => {
       req.body.targetTokenSymbol,
       req.body.targetTokenAndUSDTPoolABI,
       function (response) {
-        res.header('Access-Control-Allow-Origin', '*').status(200).send(response);
+        res.status(200).send(response);
       }
     );
   } catch (error) {
-    res.status(500).header('Access-Control-Allow-Origin', '*').send(error);
+    res.status(500).send(error);
   }
 });
 
